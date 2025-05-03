634951e090ef0fa41313d81052c08ebbabe6fa5a
Pablo Andres Aranzazu V <paaranzazuv@unal.edu.co>
2025-05-03 11:00:23 -0500

    sepraracion funciones

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -7,15 +7,6 @@ from ._internals.read_all_lines import read_all_lines
 from ._internals.split_in_words import split_in_words
 from ._internals.write_count_words import write_count_words
 
-# def read_all_lines():
-#     all_lines = []
-#     input_file_list = os.listdir("data/input/")
-#     for filename in input_file_list:
-#         with open(filename, "r", encoding="utf-8") as f:
-#             lines = f.readlines()
-#             all_lines.extend(lines)
-#     return all_lines
-
 
 def main():
 
@@ -31,16 +22,6 @@ def main():
     ## count words
     counter = count_words(words)
 
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_file_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    ##
     write_count_words(counter)
 
 
