d0598c1488b9d29aee9a5d7ab0552ff1d0a0db4b
Pablo Andres Aranzazu V <paaranzazuv@unal.edu.co>
2025-05-03 09:49:36 -0500

    version 2

--- homework/tests/test_migracion.py
+++ homework/tests/test_migracion.py
@@ -1,7 +1,11 @@
 import os
 
+from ..src.wordcount import main
+
 
 def test_migracion():
+
+    main()
 
     if not os.path.exists("data/output/results.tsv"):
         raise FileNotFoundError(" el archivo results.tvs no existe.")
