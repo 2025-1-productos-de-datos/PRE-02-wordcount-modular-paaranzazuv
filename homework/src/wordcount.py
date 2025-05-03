c8ccb28f2d91f2e646cd62db8af546e69f0b19d0
Pablo Andres Aranzazu V <paaranzazuv@unal.edu.co>
2025-05-03 10:03:21 -0500

    version 3

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,13 +1,17 @@
 # obtain a list of files in the input directory
 import os
 
-# def read_all_lines():
-# all_lines = []
-# for filename in os.listdir("data/input/"):
-# with open(filename, "r", encoding="utf-8") as f:
-# lines = f.readlines()
-# all_lines.extend(lines)
-# return all_lines
+from .write_count_words import write_count_words
+
+
+def read_all_lines():
+    all_lines = []
+    input_file_list = os.listdir("data/input/")
+    for filename in input_file_list:
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
 
 
 def main():
@@ -26,17 +30,6 @@ def main():
 
     # create the directory output/ if it doesn't exist
     write_count_words(counter)
-
-
-def write_count_words(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
-
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        for key, value in counter.items():
-            # write the key and value to the file
-            f.write(f"{key}\t{value}\n")
 
 
 if __name__ == "__main__":
